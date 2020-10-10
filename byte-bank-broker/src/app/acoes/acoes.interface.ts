350c59e19642aaca8ba1b460e6bb43a4106d5f0e
RodResq <rodrigoresque@yahoo.com.br>
2020-10-10 18:56:20 -0300

    updt

---
+++ byte-bank-broker/src/app/acoes/acoes.interface.ts
@@ -0,0 +1,13 @@
+
+export interface Acoes extends Array<Acao> {}
+
+export interface Acao {
+    id:string,
+    codigo:string,
+    descricao:string,
+    preco:string
+}
+
+export interface AcaoPayLoad {
+    
+}
